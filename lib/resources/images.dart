e7ef277d1d11fe34be9b85e22aca00b2f6fde762
GarifullinII <ildaridrisovich@gmail.com>
2022-02-11 16:59:29 +0400

    added spider

---
+++ lib/resources/images.dart
@@ -0,0 +1,7 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String imageCity = 'assets/images/image_city.jpeg';
+}
---
+++ test/images_test.dart
@@ -0,0 +1,10 @@
+// import 'dart:io';
+//
+// import 'package:movie_db/resources/resources.dart';
+// import 'package:test/test.dart';
+//
+// void main() {
+//   test('images assets test', () {
+//     expect(File(Images.imageCity).existsSync(), true);
+//   });
+// }
