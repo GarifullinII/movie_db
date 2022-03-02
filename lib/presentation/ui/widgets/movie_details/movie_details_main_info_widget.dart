88ce69352bad849924e447359f89f23ac84c32f8
GarifullinII <ildaridrisovich@gmail.com>
2022-03-02 20:35:06 +0400

    added _TopPosterWidget

--- lib/presentation/ui/widgets/movie_details/movie_details_main_info_widget.dart
+++ lib/presentation/ui/widgets/movie_details/movie_details_main_info_widget.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:movie_db/resources/resources.dart';
 
 class MovieDetailsMainInfoWidget extends StatelessWidget {
   const MovieDetailsMainInfoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        _TopPosterWidget(),
+        _MovieNameWidget(),
+      ],
+    );
+  }
+}
+
+class _TopPosterWidget extends StatelessWidget {
+  const _TopPosterWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        Image(
+          image: AssetImage(Images.kingbig),
+        ),
+        Positioned(
+          top: 20,
+          left: 20,
+          bottom: 20,
+          child: Image(
+            image: AssetImage(Images.kingsmall),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _MovieNameWidget extends StatelessWidget {
+  const _MovieNameWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
